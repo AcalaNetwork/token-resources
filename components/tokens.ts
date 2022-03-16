77b70ecb28948affa80549a1709d12d6e214e7e0
qwer951123 <qiuhao@qwer951123deMacBook-Pro.local>
2022-03-16 16:28:14 +0800

    update

---
+++ components/tokens.ts
@@ -0,0 +1,86 @@
+export const tokens = Object.values({
+  ACA: {
+    icon: '/tokens/ACA.svg',
+    symbol: 'ACA'
+  },
+  ARIS: {
+    icon: '/tokens/ARIS.png',
+    symbol: 'ARIS'
+  },
+  AUSD: {
+    icon: '/tokens/AUSD.svg',
+    symbol: 'AUSD'
+  },
+  BTC: {
+    icon: '/tokens/BTC.svg',
+    symbol: "BTC"
+  },
+  DOT: {
+    icon: '/tokens/DOT.svg',
+    symbol: 'DOT'
+  },
+  KAR: {
+    icon: '/tokens/KAR.svg',
+    symbol: 'KAR'
+  },
+  KBTC: {
+    icon: '/tokens/KBTC.png',
+    symbol: 'KBTC'
+  },
+  KINT: {
+    icon: '/tokens/KINT.png',
+    symbol: 'KINT'
+  },
+  KSM: {
+    icon: '/tokens/KSM.png',
+    symbol: 'KSM'
+  },
+  KUSD: {
+    icon: '/tokens/KUSD.svg',
+    symbol: 'KUSD'
+  },
+  LCDOT: {
+    icon: '/tokens/LDOT.svg',
+    symbol: 'LCDOT'
+  },
+  LDOT: {
+    icon: '/tokens/LDOT.svg',
+    symbol: 'LDOT'
+  },
+  LKSM: {
+    icon: '/tokens/LKSM.svg',
+    symbol: 'LKSM'
+  },
+  PHA: {
+    icon: '/tokens/PHA.svg',
+    symbol: 'PHA'
+  },
+  QTZ: {
+    icon: '/tokens/QTZ.svg',
+    symbol: 'QTZ'
+  },
+  REM: {
+    icon: '/tokens/REN.svg',
+    symbol: 'REM'
+  },
+  RMRK: {
+    icon: '/tokens/RMRK.png',
+    symbol: 'RMRK'
+  },
+  TAI: {
+    icon: '/tokens/TAI.svg',
+    symbol: 'TAI'
+  },
+  polkaBTC: {
+    icon: '/tokens/polkaBTC.svg',
+    symbol: "polkaBTC"
+  },
+  taiKSM: {
+    icon: '/tokens/taiKSM.svg',
+    symbol: 'taiKSM'
+  },
+  vsKSM: {
+    icon: '/tokens/vsKSM.svg',
+    symbol: 'vsKSM'
+  }
+})
