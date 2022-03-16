0caad3635f65403a6fe3b6998069b4ac1667aa22
qwer951123 <qiuhao951@gmail.com>
2022-03-16 17:51:27 +0800

    update

---
+++ components/chains.ts
@@ -0,0 +1,58 @@
+export const chains = Object.values({
+  acala: {
+    icon: '/network/acala.svg',
+    name: 'acala'
+  },
+  bifrost: {
+    icon: '/network/bifrost.svg',
+    name: 'bifrost'
+  },
+  hydra: {
+    icon: '/network/hydra.png',
+    name: 'hydra'
+  },
+  karura: {
+    icon: '/network/karura.svg',
+    name: "karura"
+  },
+  khala: {
+    icon: '/network/khala.svg',
+    name: 'khala'
+  },
+  kintsugi: {
+    icon: '/network/kintsugi.png',
+    name: 'kintsugi'
+  },
+  kusama: {
+    icon: '/network/kusama.png',
+    name: 'kusama'
+  },
+  laminar: {
+    icon: '/network/laminar.svg',
+    name: 'laminar'
+  },
+  phala: {
+    icon: '/network/phala.svg',
+    name: 'phala'
+  },
+  plasm: {
+    icon: '/network/plasm.svg',
+    name: 'plasm'
+  },
+  polkaBTC: {
+    icon: '/network/polkaBTC.svg',
+    name: 'polkaBTC'
+  },
+  polkadot: {
+    icon: '/network/polkadot.svg',
+    name: 'polkadot'
+  },
+  rococo: {
+    icon: '/network/rococo.svg',
+    name: 'rococo'
+  },
+  statemine: {
+    icon: '/network/statemine.svg',
+    name: 'statemine'
+  },
+})
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,29 @@
 import Image from "next/image";
 import { tokens } from "../components/tokens.ts";
+import { chains } from "../components/chains.ts";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import Head from "next/head";
 import { useMemo } from "react";
 
-export default function Home() {
-  const hostname = useMemo(() => {
-    if (typeof window !== "undefined") return window.location.hostname;
+const PUBLIC_HOST = 'https://resources.acala.network';
 
-    return "";
-  }, []);
+export default function Home() {
+  const hostname = PUBLIC_HOST;
 
   return (
     <>
       <Head>
-        <title>Acala Token Resource</title>
+        <title>Acala Resources</title>
       </Head>
       <section className="p-16 bg-slate-900 min-w-screen min-h-screen">
         <h1 className="text-2xl  text-red-400 font-bold mb-16">
-          Acala Token Resource
+          Acala Tokens
         </h1>
 
         <ul className="flex flex-wrap gap-12">
           {tokens.map((item) => {
             const file = `https://${hostname}${item.icon}`;
             const temp = item.icon.split("/");
-
-            console.log(temp);
 
             return (
               <a
@@ -42,9 +39,43 @@ export default function Home() {
                     objectFit="contain"
                     width="48"
                     height="48"
+                    className="max-w-[48px]"
                   />
                   <p className="text-md text-slate-300 mt-1 text-center">
                     {item.symbol}
+                  </p>
+                </li>
+              </a>
+            );
+          })}
+        </ul>
+
+        <h1 className="text-2xl  text-red-400 font-bold mb-16 mt-32">
+          Chain Icons
+        </h1>
+
+        <ul className="flex flex-wrap gap-12">
+          {chains.map((item) => {
+            const file = `https://${hostname}${item.icon}`;
+            const temp = item.icon.split("/");
+
+            return (
+              <a
+                key={item.name}
+                href={file}
+                download={temp[temp.length - 1]}
+                className="text-xs text-slate-300 mt-1 text-center"
+              >
+                <li className="border-1 border-slate-300 flex-[96px] flex-grow-0 flex-shrink-0 w-[96px] h-[96px] flex justify-center align-center flex-col hover:bg-slate-600 transition-all rounded-md cursor-pointer">
+                  <Image
+                    src={item.icon}
+                    alt={item.name}
+                    objectFit='contain'
+                    width={48}
+                    height={48}
+                  />
+                  <p className="text-md text-slate-300 mt-1 text-center">
+                    {item.name}
                   </p>
                 </li>
               </a>
